399aca328101f31dd16cb1eb1d95f842d1e2df82
qarani-m <emqarani@gmail.com>
2024-03-12 03:37:24 +0300

    Managed to generate an authentication token

--- src/main/java/com/mkarani/stkpush/utils/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.mkarani.stkpush.utils;
-
-public class Constants {
-    public static final String BASIC_AUTH_STRING = "Basic";
-    public static final String AUTHORIZATION_HEADER_STRING = "Authorization";
-    public static final String CACHE_CONTROL_HEADER = "cache-control";
-    public static final String CACHE_CONTROL_HEADER_VALUE = "no-cache";
-
-}
